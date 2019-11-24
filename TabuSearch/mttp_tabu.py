c998d86a19a1dfc0016fed842667572bcb2fd2f5
RafaAudibert <rafaeelaudibert@gmail.com>
2019-11-24 08:17:50 -0300

    :recycle::sparkles: Updated tabu search

--- TabuSearch/mttp_tabu.py
+++ TabuSearch/mttp_tabu.py
@@ -1,17 +1,18 @@
-import argparse
-
 from neighbourhood import *
 
+import argparse
+import copy
+from random import seed
+
+
 def read_instance(instance_file):
-    distances_matrix = []
-    f = open(instance_file, "r")
-    for line in f:
-        row = line.rstrip("\n\r")
-        row_values = [int(s) for s in row.split() if s.isdigit()]
-        distances_matrix.append(row_values)
+    distance_matrix = []
+    with open(instance_file, "r") as f:
+        for line in f:
+            distance_matrix.append([int(s) for s in line.rstrip(
+                "\n\r").split() if s.isdigit()])
 
-
-    schedule_matrix = [row[:] for row in distances_matrix]
+    schedule_matrix = [row[:] for row in distance_matrix]
 
     teams = len(schedule_matrix)
     row_size = len(schedule_matrix)
@@ -21,92 +22,75 @@ def read_instance(instance_file):
         for j in range(column_size):
             schedule_matrix[i][j] = None
 
-
-    return distances_matrix, schedule_matrix, teams
-
-def main(distances_matrix, schedule_matrix_original, teams, neighbourhood_size, idle_iterations):
-
-    games_location_matrix, schedule_matrix = calculate_initial_solution(schedule_matrix_original, teams)
+    return distance_matrix, schedule_matrix, teams
 
 
-    print('Distances matrix')
-    for l in distances_matrix:
-        print(l)
-
-    print('\nMatchups')
-    for l in schedule_matrix:
-        print(l)
-
-    print('\nLocations')
-    for row in games_location_matrix:
-        print(row)
-
-
-
-
-#TODO: Transferir main() para tabu_search
-def tabu_search(arg):
+def tabu_search(distance_matrix, model_schedule_matrix, teams, neighbourhood_size, max_iterations=15):
     # Step 0: create variables attached to tabu search logic implementation
-    graph = [] # configuração do grafo de entrada *Na entrada que for definida/ED*
+    rounds = (teams - 1) * 2
     iteration = best_iteration = 0
-    best_solution = [] # irá guardar a melhor solução
-    tabu_list = [] # lista tabu inicialmente vazia
-    #capacidade_maxima = 23  Acredito que nao se aplica a nosso caso
-
-    # TODO: criterio parada
-    bt_max = 5 # quantidade máxima de iterações sem melhora no valor da melhor solução
-    #TODO: maximo vizinhos (?)
-        #max_vizinhos = 5 # quantidade máxima de vizinhos
+    best_solution_schedule, best_solution_location = [], []  # will store best solution
+    best_solution = 0
+    tabu_list = []  # initially empty tabu list
 
     # Step 1: Generate a random initial solution
-    initial_solution = calculate_initial_solution()
-    best_solution = initial_solution
+    schedule_matrix, location_matrix = get_initial_solution(
+        model_schedule_matrix, teams)
+    best_solution_schedule, best_solution_location = schedule_matrix, location_matrix
 
-    objective_function_value = evaluate_instance_objective_function(graph, initial_solution)
-
-    # Generate neighbours (neighbourhood)
-    neighbours = generate_neighbours(best_solution, max_vizinhos, tabu_list)
-
-    # Calculate neighbours avaliation
-    neighbours_avaliation = evaluate_neighbours(neighbourhood, graph, max_vizinhos)
-
-    # Obtain index of the best neighbour
-    index_best_neighbour = get_index_best_neighbour(neighbours_avaliation, tabu_list, best_solution, neighbours)
-
-    # Check if best neighbour has better avalation than the current best solution
-    # TODO: decide how to define the "diff", which data structure
-    if neighbours[index_best_neighbour] > best_solution:
-        diff =  get_diff(best_solution, neighbours[index_best_neighbour])
-        tabu_list.append(diff)
-        best_solution = neighbours[index_best_neighbour][:] #copy best solution
-        best_iteration +=1
+    best_solution = compute_obj_function(
+        distance_matrix, schedule_matrix, location_matrix, teams, rounds)
+    print(f"Initial solution is {best_solution}")
 
     # Enf of Step1
 
-    # Step2: iterate the generation of neighours until $(bt_max) consecutive iterations do not find a new best solution
-    while iteration - best_iteration <= bt_max:
+    # Step2: iterate the generation of neighours until $(max_iterations) consecutive iterations do not find a new best solution
+    while iteration - best_iteration <= max_iterations:
+        iteration += 1
+        print(f"Started iteration {iteration}")
+
         # Generate neighbours (neighbourhood)
-        neighbours = generate_neighbours(best_solution, max_vizinhos, tabu_list)
+        neighbours = generate_neighbours(
+            best_solution_schedule, best_solution_location, neighbourhood_size, tabu_list)
 
         # Calculate neighbours avaliation
-        neighbours_avaliation = evaluate_neighbours(neighbourhood, graph, max_vizinhos)
+        neighbours_avaliation = [compute_obj_function(
+            distance_matrix, n_schedule, n_location, teams, rounds) for n_schedule, n_location, _n_change in zip(*neighbours)]
 
         # Obtain index of the best neighbour
-        index_best_neighbour = get_index_best_neighbour(neighbours_avaliation, tabu_list, best_solution, neighbours)
+        best_neighbour_idx = neighbours_avaliation.index(
+            min(neighbours_avaliation))
 
         # Check if best neighbour has better avalation than the current best solution
-        # TODO: decide how to define the "diff", which data structure
-        if neighbours[index_best_neighbour] > best_solution:
-            diff =  get_diff(best_solution, neighbours[index_best_neighbour])
+        if min(neighbours_avaliation) < best_solution:
+            diff = neighbours[CHANGE][best_neighbour_idx]
             tabu_list.append(diff)
-            best_solution = neighbours[index_best_neighbour][:] #copy best solution
-            best_iteration +=1
+
+            # copy best solution
+            best_solution_schedule = copy.deepcopy(
+                neighbours[SCHEDULE][best_neighbour_idx])
+            best_solution_location = copy.deepcopy(
+                neighbours[LOCATION][best_neighbour_idx])
+            best_solution = min(neighbours_avaliation)
+            best_iteration = iteration
+            print(f"Improved on iteration {best_iteration} to {best_solution}")
+
+    print(f"Final solution is {best_solution}")
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process path')
-    parser.add_argument('-f', '--txt', required=True, type=str, help='Input instance txt path')
-    parser.add_argument('-n', '--size', required=True, type=int, help='Input neighbourhood size')
-    parser.add_argument('-i', '--iter', required=True, type=int, help='Input iterations')
+    parser.add_argument('-f', '--txt', required=True,
+                        type=str, help='Input instance txt path')
+    parser.add_argument('-n', '--neighbourhood-size', required=True,
+                        type=int, help='Input neighbourhood size')
+    parser.add_argument('-i', '--iter', required=True,
+                        type=int, help='Input iterations')
+    parser.add_argument('-s', '--seed', default=1,
+                        type=int, help='Random seed number')
     args = parser.parse_args()
-    distances_matrix, schedule_matrix, teams = read_instance(args.txt)
-    main(distances_matrix, schedule_matrix, teams, args.size, args.iter)
+
+    seed(args.seed)
+    distance_matrix, model_schedule_matrix, teams = read_instance(args.txt)
+    tabu_search(distance_matrix, model_schedule_matrix, teams,
+                args.neighbourhood_size, args.iter)
